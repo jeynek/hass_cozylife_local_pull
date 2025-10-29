5e07371b5a20275ec6b68dc1c62f965a44c47de1
jeynek <41385376+jeynek@users.noreply.github.com>
2025-10-29 13:20:55 +0100

    Update const.py

--- custom_components/hass_cozylife_local_pull/const.py
+++ custom_components/hass_cozylife_local_pull/const.py
@@ -1,33 +1,34 @@
+"""Constants for the CozyLife Local Pull integration."""
+
 DOMAIN = "hass_cozylife_local_pull"
 
-# http://doc.doit/project-5/doc-8/
+SIGNAL_NEW_TCP_CLIENT = f"{DOMAIN}_new_tcp_client"
+
+CONF_LANG = "lang"
+CONF_IP_ADDRESS = "ip_address"
+
+# Device type codes
 SWITCH_TYPE_CODE = '00'
 LIGHT_TYPE_CODE = '01'
-# sensor type code (placeholder; keep if some devices report sensor-type code)
 SENSOR_TYPE_CODE = '02'
 SUPPORT_DEVICE_CATEGORY = [SWITCH_TYPE_CODE, LIGHT_TYPE_CODE, SENSOR_TYPE_CODE]
 
-# Device DPIDs (kept as strings to match device message keys)
+# Device DPIDs (strings)
 SWITCH = '1'
 WORK_MODE = '2'
-
-# Updated DPIDs & scaling based on iam-medvedev/homebridge-cozylife-temperature-sensor:
-# - Temperature: DPID "8" (value = temperature * 10)
-# - Humidity:  DPID "4" (value = humidity %)
-# - Battery:   DPID "9" (value = battery * 10)
-TEMP = '8'
-HUMIDITY = '4'
-BATTERY = '9'
-
-# Legacy/light DPIDs (values kept as strings)
+COLOR_TEMP = '3'  # Color temperature for lights (0-1000)
 BRIGHT = '4'
-HUE = '5'
-SAT = '6'
+HUE = '5'  # 0-65535 (scale to 0-360)
+SAT = '6'  # 0-65535 (scale to 0-1000)
 
-LIGHT_DPID = [SWITCH, WORK_MODE, TEMP, BRIGHT, HUE, SAT]
-SWITCH_DPID = [SWITCH, ]
-# Sensor DPID list (temperature, humidity, battery)
-SENSOR_DPID = [TEMP, HUMIDITY, BATTERY]
+# Sensor DPIDs
+SENSOR_TEMP = '8'  # Temperature *10
+HUMIDITY = '4'  # Humidity % (or *10)
+BATTERY = '9'  # Battery *10
+
+LIGHT_DPID = [SWITCH, WORK_MODE, COLOR_TEMP, BRIGHT, HUE, SAT]
+SWITCH_DPID = [SWITCH]
+SENSOR_DPID = [SENSOR_TEMP, HUMIDITY, BATTERY]
 
 LANG = 'en'
 API_DOMAIN = 'api-us.doiting.com'
