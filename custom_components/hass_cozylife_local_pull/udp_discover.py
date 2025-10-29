d62d99f6fb4422af6ffb04f722938eb3ad05e642
jeynek <41385376+jeynek@users.noreply.github.com>
2025-10-29 13:27:34 +0100

    Update udp_discover.py

--- custom_components/hass_cozylife_local_pull/udp_discover.py
+++ custom_components/hass_cozylife_local_pull/udp_discover.py
@@ -1,67 +1,33 @@
-import socket
+import asyncio
 import time
-from .utils import get_sn
 import logging
 
+from .utils import get_sn
 
 _LOGGER = logging.getLogger(__name__)
 
-"""
-discover device
-"""
+
+class DiscoveryProtocol(asyncio.DatagramProtocol):
+    def __init__(self):
+        self.transport = None
+        self.queue = asyncio.Queue()
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        self.queue.put_nowait((data, addr))
 
 
-def get_ip() -> list:
+async def async_get_ips(timeout=5) -> list:
     """
-    get device ip
-    :return: list
+    Discover device IPs via UDP broadcast.
+    :return: list of unique IPs
     """
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # server.bind(('192.168.123.1', 0))
-    # Enable broadcasting mode
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    
-    # Set a timeout so the socket does not block
-    # indefinitely when trying to receive data.
-    server.settimeout(0.1)
-    socket.setdefaulttimeout(0.1)
-    message = '{"cmd":0,"pv":0,"sn":"' + get_sn() + '","msg":{}}'
-    
-    i = 0
-    while i < 3:
-        # server.sendto(bytes(message, encoding='utf-8'), ('<broadcast>', 6095))
-        server.sendto(bytes(message, encoding='utf-8'), ('255.255.255.255', 6095))
-        time.sleep(0.03)
-        i += 1
-
-    # max tries before first data received
-    max = 5
-    i = 0
-    while i < max:
-        i += 1
-        try:
-            data, addr = server.recvfrom(1024, socket.MSG_PEEK)
-        except Exception as err:
-            _LOGGER.info(f'{i}/{max} try, udp timeout')
-            continue
-        _LOGGER.info(f'first udp.receiver:{addr[0]}')
-        break
-    else:
-        _LOGGER.warning('cannot find any device')
-        return []
-    
-    i = 255
-    ip = []
-    while i > 0:
-        try:
-            data, addr = server.recvfrom(1024)
-        except:
-            _LOGGER.info('udp timeout')
-            break
-        _LOGGER.info(f'udp.receiver:{addr[0]}')
-        if addr[0] not in ip: ip.append(addr[0])
-        i -= 1
-    
-    return ip
+    loop = asyncio.get_running_loop()
+    transport, protocol = await loop.create_datagram_endpoint(
+        DiscoveryProtocol,
+        local_addr=('0.0.0.0', 0),
+        allow_broadcast=True
+    )
+    transport.sendto(b
